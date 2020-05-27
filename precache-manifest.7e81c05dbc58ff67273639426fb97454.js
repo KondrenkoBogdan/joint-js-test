5c064fcf69d2ef0d8dd7f65f1e04f9c1160925fc
Bogdan Kondrenko <kondrenkobogdan2808@gmail.com>
2020-05-27 12:14:36 +0300

    Updates

---
+++ precache-manifest.7e81c05dbc58ff67273639426fb97454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc12320150296c54c84248630488c298",
+    "url": "/joint-js-test/index.html"
+  },
+  {
+    "revision": "c3c753ebf4e462a6946f",
+    "url": "/joint-js-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ecc0ef971685d997b48",
+    "url": "/joint-js-test/static/js/2.300b32b4.chunk.js"
+  },
+  {
+    "revision": "ef56f730c405e4a91c5870487ddc63ba",
+    "url": "/joint-js-test/static/js/2.300b32b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c753ebf4e462a6946f",
+    "url": "/joint-js-test/static/js/main.7496566f.chunk.js"
+  },
+  {
+    "revision": "b32001e5c4b08bb9031e",
+    "url": "/joint-js-test/static/js/runtime-main.743da94e.js"
+  }
+]);
